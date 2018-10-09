4ef65994e5774e1e8f1b4a4ad7237cae5e4cf26d
Daniel Fratila <>
2018-10-09 20:57:49 +0300

    profileDesignDone

--- AuctionApp/Controllers/ProfileViewController.swift
+++ AuctionApp/Controllers/ProfileViewController.swift
@@ -7,17 +7,31 @@
 //
 
 import UIKit
+import Photos
 
-class ProfileViewController: UIViewController,UITabBarDelegate, UIImagePickerControllerDelegate {
-    @IBOutlet weak var profilePicture: UIImageView!
+class ProfileViewController: UIViewController,UITabBarDelegate, UIImagePickerControllerDelegate , UINavigationControllerDelegate {
+    
+    @IBOutlet weak var profilePicture: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var tabBar: UITabBar!
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBar.delegate = self
-        // Do any additional setup after loading the view.
+        self.imagePicker.delegate = self
+        checkPermission()
+        setProfilePictureImageAttributes()
+        
+    }
+    
+    func setProfilePictureImageAttributes(){
+        profilePicture.layer.cornerRadius = 0.5 * profilePicture.bounds.size.width
+        profilePicture.layer.borderWidth = 5.0
+        var customGreen = UIColor(red: 95, green: 232, blue: 194, alpha: 0.5)
+        profilePicture.layer.borderColor = customGreen.cgColor
+        profilePicture.translatesAutoresizingMaskIntoConstraints = true
     }
     
     override var prefersStatusBarHidden: Bool { return true }
@@ -36,6 +50,52 @@ class ProfileViewController: UIViewController,UITabBarDelegate, UIImagePickerCon
             print("Otherwise, do something else.")
         }
         
+    }
+    
+    @IBAction func addImage(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        profilePicture.imageView?.contentMode = .scaleAspectFill
+        profilePicture.setImage(selectedImage, for: .normal)
+        
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func checkPermission() {
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+        switch photoAuthorizationStatus {
+        case .authorized:
+            print("Access is granted by user")
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization({
+                (newStatus) in
+                print("status is \(newStatus)")
+                if newStatus ==  PHAuthorizationStatus.authorized {
+                    /* do stuff here */
+                    print("success")
+                }
+            })
+            print("It is not determined until now")
+        case .restricted:
+            // same same
+            print("User do not have access to photo album.")
+        case .denied:
+            // same same
+            print("User has denied the permission.")
+        }
     }
     
     @IBAction func modifyName(_ sender: Any) {
